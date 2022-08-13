41f1e42fcbbadcc7c14e6cb690286522fc293ad2
billferrabone <billferrabone@gmail.com>
2022-08-13 03:08:53 +0000

    logica de cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+let palos = ["♦", "♥", "♠", "♣"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cards_x = Math.floor(Math.random() * cards.length);
+  let palos_x = Math.floor(Math.random() * palos.length);
+  let color = palos[palos_x] == "♦" || palos[palos_x] == "♥" ? "red" : "black";
+
+  document.write(palos[palos_x] + " " + cards[cards_x] + " " + palos[palos_x]);
 };
+// onload();
